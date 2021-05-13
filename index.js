a8999cb95e0af55e3af99e784bd2afb75b142861
ttaylor05 <tmjackson0712@gmail.com>
2021-05-13 18:21:24 -0400

    update

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function getNoteId() {
-    let noteObject = getExistingNotes()
+    let noteObject = getExistingNotes();
     if(!noteObject){
-            return 1
+            return 1;
     }
-    const keysArray = Object.keys(noteObject)
-    const numberKeys = keysArray.map((key)=>Number (key))
-    console.log(numberKeys)
-    return Math.max( ...numberKeys) + 1
+    const keysArray = Object.keys(noteObject);
+    const numberKeys = keysArray.map((key) => Number (key));
+    console.log(numberKeys);
+    return Math.max( ... numberKeys) + 1;
 }
 function getExistingNotes() {
-    let notes = localStorage.getItem('notes')
+    let notes = localStorage.getItem('notes');
     if(!notes){
         return null
     }
-    return JSON.parse(notes)
+    return JSON.parse(notes);
 }
